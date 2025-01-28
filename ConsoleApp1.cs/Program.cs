736e0df1bd9a7ee3950b5b24222ca03370dfb181
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-01-28 20:11:20 +0000

    First Steps
    Simple program where I explore variables

---
+++ ConsoleApp1.cs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Program.cs
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("How old are you?");
+            string input;
+            input = Console.ReadLine();
+            
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Your name is {name}. You are {input} years old.");
+            
+            
+            // Create Variables
+            string name2;
+            string breed;
+            int age = 5;
+            double weight = 65.22;
+            bool spayed = true;
+
+            name2 = "Shadow";
+            breed = "Golden Retriever";
+
+            // Print variables to the console
+            Console.WriteLine($"{name2}");
+            Console.WriteLine($"{breed}");
+            Console.WriteLine($"{age}");
+            Console.WriteLine($"{weight}");
+            Console.WriteLine($"{spayed}");
+        }
+    }
+}
