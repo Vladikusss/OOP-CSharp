109139e44254806cd94401634b752ddc47c7bc94
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-02-06 16:09:21 +0000

    Random Number Generator

--- ConsoleApp3/App3.cs
+++ ConsoleApp3/App3.cs
@@ -1,24 +1,36 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace ConsoleApp3
-{
+{ // Program to generate random numbers
     class Another
     {
-        static void main()
+        static void Main()
         {
             GenerateRandomIntegers();
-
         }
-        
+
         static void GenerateRandomIntegers()
         {
             Console.WriteLine("Started");
-            List<int> = new list<int>();
+            List<int> numbers = new List<int>(); // List data type from System.Collections.Generic
             
-            Console.WriteLine("Ended")''
+            Random random = new Random(); // An Instance of a class Random
+
+            for (int i = 0; i < 100; i++)
+            { // Generates 100 random numbers
+                numbers.Add(random.Next(1, 1001));
+            }
+
+            for (int x = 0; x < numbers.Count; x++)
+            { // Output generated numbers to the console
+                Console.Write(numbers[x] + ", ");
+                if ((x + 1) % 10 == 0)
+                { // Every 10 numbers, a new line is added
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine("Ended");
         }
-        
     }
 }
-
