c2ee29b80fdfb2dffdd0516b3b1ba685d22c11ec
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-02-11 15:59:24 +0000

    Implemented Insert Search Algorithm
    Simple insert search algorithm that works like card shuffling. Firstly implemented in python to translate in c#

--- ConsoleApp3/App3.py
+++ ConsoleApp3/App3.py
@@ -17,6 +17,28 @@ def main():
     histogram = ComputeHistogram(integers)
     DisplayHistogram(histogram)
 
+    # Test the function
+    arr = [5, 3, 8, 4, 2, 25, 23, 867, 345, 23423423, 765, 293, 5, 345, 56478, 6, 98765, 645, 13, 68, 23]
+    print("Before Sorting:", arr)
+    insertion_sort(arr)
+    print("\nAfter Sorting:", arr)
+
+def insertion_sort(numbers):
+    """Insertion sort algorithm to sort out array of numbers."""
+    for i in range(1, len(numbers)): # start at index 1, as index 0 is assumed to be sorted
+        currentElement = i # index from outer loop of current element
+        beforeElement = i - 1 # index of the value before current index
+        # while index of previous element is >=0 and value of previous element >= current element
+        while beforeElement >= 0 and numbers[beforeElement] > numbers[currentElement]:
+            numbers[beforeElement + 1] = numbers[beforeElement] # move previous element one step forward, e.g. 0->1
+            beforeElement -= 1 # decrement by 1 to go to previous of previous element
+        numbers[beforeElement + 1] = numbers[currentElement] # increment by 1 to put current element in correct position
+
+
+
+
+
+
 def GenerateRandomIntegers() -> list:
     """Function to generate 100 numbers"""
 
