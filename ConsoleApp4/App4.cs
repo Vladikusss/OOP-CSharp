c54968b7f5077947b80e433bae7b675a0752a806
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-02-12 10:15:31 +0000

    Created user-defined class that holds studenrt info
    First time creating my own class. Very simple so far, but will be expanded

--- ConsoleApp4/App4.cs
+++ ConsoleApp4/App4.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
 
-Console.WriteLine("Hello, World!");
+
+namespace ConsoleApp4
+{
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Module;
+        public int Grade;
+        public static int NumberOfStudents = 0;
+
+        public Student(string studentName, string studentSurname, string studentModule, int studentGrade)
+        {
+            Name = studentName;
+            Surname = studentSurname;
+            Module = studentModule;
+            Grade = studentGrade;
+            NumberOfStudents++;
+        }
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(
+                "John",
+                "Doe",
+                "Object-Oriented Programming",
+                73);
+        }
+        
+    }
+}
