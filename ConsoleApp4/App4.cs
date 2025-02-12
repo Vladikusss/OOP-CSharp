86001b015f9f5460a0e46b2f34edb11db17d3f9c
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-02-12 10:47:13 +0000

    Modified the class
    Added if-statements and learned about getters

--- ConsoleApp4/App4.cs
+++ ConsoleApp4/App4.cs
@@ -12,6 +12,19 @@ namespace ConsoleApp4
         public int Grade;
         public static int NumberOfStudents = 0;
 
+        public string FullName
+        {
+            get { return Name + " " + Surname; }
+        }
+        
+        /* Declaration above is traditional get method. 
+         From c# version 6.0 (2015) the new method was introduced:
+         public string FullName => Name + " " + Surname;
+         
+         get {} is preferred when validation is needed before assigning a value - i.e. chains of logic.
+         => expression-bodied for simple expressions to make code more readable. 
+        */
+
         public Student(string studentName, string studentSurname, string studentModule, int studentGrade)
         {
             Name = studentName;
@@ -32,6 +45,30 @@ namespace ConsoleApp4
                 "Doe",
                 "Object-Oriented Programming",
                 73);
+            
+            Console.WriteLine($"{student1.Name}, welcome to the module {student1.Module}!\n" +
+                              $"Your current grade is {student1.Grade}. Let's see if you can improve it!");
+            Console.WriteLine($"{student1.FullName}, do you want to improve your grade? y/n");
+            string userInput = Console.ReadLine().ToLower();
+            if (userInput == "y")
+            {
+                Console.WriteLine($"You can improve your grade by doing the following:\n" +
+                                  $"1. Focus on Understanding Code Design and Architecture.\n" +
+                                  $"2. Master Core OOP Concepts.\n" +
+                                  $"3. Practice Coding Regularly.\n");
+            }
+            else if (userInput == "n")
+            {
+                Console.WriteLine($"How come you don't want to improve your grade?");
+                Console.ReadLine();
+                Console.WriteLine($"Okay. I understand you, {student1.Name}.\n" +
+                                  $"At the end of the day success is all that matters.");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid input next time!");
+            }
+            
         }
         
     }
