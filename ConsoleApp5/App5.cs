d66df901c8dae11f6ccfb24f88e59296cee71891
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-02-13 15:32:05 +0000

    Create App5 - Algorithms
    Here I will analyse a variety of algorithms and figure out (estimate) their time complexity based on Big(O) notation

---
+++ ConsoleApp5/App5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 1.5 * 20; i++)
+            { // O(n) - execution time grows in direct proportion to input (linear growth)
+                Console.Write($"{i}, ");
+            }
+            Console.WriteLine();
+            for (int i = 20; i >= 1; i--)
+            { // O(n) - execution time grows in direct proportion to input (linear growth)
+                Console.Write($"{i}, ");
+            }
+        }
+}
+}
