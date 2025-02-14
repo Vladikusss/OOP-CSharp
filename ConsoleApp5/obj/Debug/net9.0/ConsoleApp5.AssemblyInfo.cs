c3730dea2d73832988c05fddd2d3692a72dd2339
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-02-14 00:29:25 +0000

    Complier changes

--- ConsoleApp5/obj/Debug/net9.0/ConsoleApp5.AssemblyInfo.cs
+++ ConsoleApp5/obj/Debug/net9.0/ConsoleApp5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d66df901c8dae11f6ccfb24f88e59296cee71891")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0391d37003de3d242c49be5c57764672a2a1f981")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
