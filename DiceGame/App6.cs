ddf12dd991acd6a3ba366ebd06498cac1e938052
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-02-14 00:29:10 +0000

    Start of a Dice Game
    In this project - I will create a simple dice game. This is the initial point of the project.

---
+++ DiceGame/App6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiceGame
+{
+    internal class Game
+    {
+        private string _title; // Private property inside the class
+        public string Title => _title; // Read-only property -> private property needs changing for new value
+
+        public Game() // Class constructor
+        {
+            _title = "DiceGame v1.01"; // Title of the game 
+        }
+
+        public void StartGame()
+        {
+            Console.WriteLine($"{Title} successfully started."); // Output to the user
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game Session = new Game(); // Instance of the Game class
+            
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(); // Await for user input before closing the console. 
+        }
+    }
+    
+}
