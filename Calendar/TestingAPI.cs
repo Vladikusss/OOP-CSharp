7763553ac7642499bd8ba9dad0385099651ee358
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-02-23 23:19:57 +0000

    Learn to make API calls in c#

---
+++ Calendar/TestingAPI.cs
@@ -0,0 +1,22 @@
+/*
+ * Created by Vladdy | 23.02.2025
+ * Last Updated: 23.02.2025
+ * Program: API Testing in c#
+ * Version: 1.0
+ * Status: In progress
+ */
+
+using System;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace Calendar
+{
+    internal class TestProgram()
+    {
+        static void Test()
+        {
+            
+        }
+    }
+}
