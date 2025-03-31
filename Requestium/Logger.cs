09eb1e716a5312b26d67563f7b0e037e01fb8a4f
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-03-31 01:14:20 +0100

    New Project to practice requests

---
+++ Requestium/Logger.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+
+namespace Requestium;
+
+internal class Logger
+{
+    private static string _root = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+    private static string _logFile = Path.Combine(_root, "logs.txt");
+    private static string logMessage;
+    
+    static DateTime timestamp = DateTime.Now;
+
+    internal static void Log(string level, string message, string? extra = null)
+    {
+        if (extra != null) {
+            logMessage = $"{timestamp} -- {level} -- {message}: {extra}";
+        } else {
+            logMessage = $"{timestamp} -- {level} -- {message}";
+        }
+
+        using (StreamWriter writeObj = new StreamWriter(_logFile, append: true))
+        {
+            writeObj.WriteLine(logMessage);
+        }
+    }
+
+    internal static void ClearLogs()
+    {
+        using (StreamWriter writeObj = new StreamWriter(_logFile, append: false))
+        {
+            writeObj.WriteLine("Log file has been Cleared at " + timestamp + '\n');
+        }
+    }
+}
---
+++ Requestium/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http; // HttpClient lives here
+using System.Threading.Tasks;
+using System.IO; // Used for logging
+
+
+namespace Requestium; 
+
+internal class Program {
+    
+    static async Task Main()
+    {
+        Logger.ClearLogs();
+        using (HttpClient browserClient = new HttpClient())
+        {   
+            Logger.Log("INFO", "Client Started.");
+            string url = "https://jsonplaceholder.typicode.com/pots/1";
+            HttpResponseMessage response = await browserClient.GetAsync(url);
+            
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(data);
+                Logger.Log("INFO", "Successfully retrieved.");
+            }
+            else 
+            { 
+                Logger.Log("ERROR", "Something went wrong with GET request", response.StatusCode.ToString());
+            }
+
+        }
+    }
+}
