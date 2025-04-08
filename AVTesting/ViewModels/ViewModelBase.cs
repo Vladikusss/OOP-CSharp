fde8801b9aea1255bba3dce274ddc23914422351
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-04-08 17:05:19 +0100

    Create AVTesting
    New directory for Avalonia UA learning

---
+++ AVTesting/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AVTesting.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ AVTesting/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AVTesting.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
